cdba8d84a3735a24f0db874d7c26f5817691b173
jamshid <jamshidtulaganov3101@gmail.com>
2023-09-25 19:42:05 +0500

    feature: vue-router initialized

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,5 @@
 </script>
 
 <template>
-    <h1 class="text-4xl font-rubik  mt-5">Alik</h1>
-    <h1 class="text-4xl font-sf-pro-text font">Alik</h1>
-    <h1 class="text-4xl font-sf-pro-display">Alik</h1>
+    <router-view></router-view>
 </template>
