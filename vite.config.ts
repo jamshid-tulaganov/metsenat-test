f0fd5da64fccd0fbd1c8e49e839570c09deafcca
jamshid <jamshidtulaganov3101@gmail.com>
2023-09-26 00:59:47 +0500

    chore: lodash types added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    checker({
+      vueTsc: true
+    })
   ],
 
 //  server
