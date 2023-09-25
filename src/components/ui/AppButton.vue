d6e68b91b6ab6a13dfa1983868e76b3cd6b1103c
jamshid <jamshidtulaganov3101@gmail.com>
2023-09-25 19:41:16 +0500

    feature: AppButton ui component initialized

---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ButtonTypes } from '../../types/components/button.type';
+
+defineProps<{
+    buttonType: ButtonTypes
+}>()
+</script>
+
+
+<template>
+    <button :type="buttonType" class="w-full pt-[14px] pb-[15px] bg-blue-800 font-rubik text-white text-base leading-[21px]
+        font-medium text-center rounded-md outline-double hover:bg-blue-700">
+        <slot />
+    </button>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "./style/main.css";
+// router
+import router from "./router/index"
 
-createApp(App).mount('#app')
+// global components
+import AppInput from "./components/ui/AppInput.vue";
+import AppButton from './components/ui/AppButton.vue';
+
+createApp(App)
+    .use(router)
+    .component("AppInput", AppInput)
+    .component("AppButton", AppButton)
+    .mount('#app')
