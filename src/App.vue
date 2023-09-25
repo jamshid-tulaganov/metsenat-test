4f69a636b76444da19f8293cf7cfc7d7ec9c10a8
jamshid <jamshidtulaganov3101@gmail.com>
2023-09-25 17:53:45 +0500

    fix: removed extra things

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,7 @@
 </script>
 
 <template>
+    <h1 class="text-4xl font-rubik  mt-5">Alik</h1>
+    <h1 class="text-4xl font-sf-pro-text font">Alik</h1>
+    <h1 class="text-4xl font-sf-pro-display">Alik</h1>
 </template>
