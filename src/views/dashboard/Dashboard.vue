744beeeecffdd388296202af4997c97a4977f012
jamshid <jamshidtulaganov3101@gmail.com>
2023-09-25 21:29:35 +0500

    feature: Dashboard UI created

---
+++ src/views/dashboard/Dashboard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DashboardHeader from './DashboardHeader.vue';
+
+</script>
+
+<template>
+    <main>
+        <DashboardHeader />
+    </main>
+</template>
---
+++ src/views/dashboard/DashboardHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import AppTabs from '../../components/base/AppTabs.vue';
+
+const search = ""
+</script>
+
+<template>
+    <header class="mt-[64px] bg-white py-6">
+        <div class="container mx-auto flex items-center">
+            <!-- tabs -->
+            <AppTabs></AppTabs>
+
+            <div class="flex items-center ml-auto">
+                <!-- searching -->
+                <AppInput
+                    v-model="search"
+                    placeholder="Izlash"
+                    isIcon
+                    class="h-10 w-[284px] mr-5 px-[11px] bg-gray-800 placeholder:text-gray-900 placeholder:text-base"
+                >
+                </AppInput>
+
+                <!-- filters -->
+
+                <div class="w-[123px] h-10 flex items-center justify-center bg-[#EDF1FD] rounded-md cursor-pointer">
+                    <img src="/assets/images/icons/filter.svg" alt="Filter Icon">
+                    <p class="ml-3 text-sf-pro-text text-[#3365FC] text-sm font-medium leading-6">Filter</p>
+                </div>
+            </div>
+            
+        </div>
+    </header>
+</template>
