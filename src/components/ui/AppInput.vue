d5b527113235d92cbd20fff945632adc2a6284e3
jamshid <jamshidtulaganov3101@gmail.com>
2023-09-25 19:20:34 +0500

    feature: AppInput ui component added

---
+++ src/components/ui/AppInput.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        modelValue: number | string | undefined;
+        inputType?: string;
+        placeholder?: string;
+        label?: string;
+    }>(),
+        {
+        inputType: "text",
+        intent: "primary",
+        labelStyle: "primary",
+    }
+);
+
+// emits
+const emit = defineEmits<{
+    (e: "update:modelValue", payload: string): void;
+}>();
+</script>
+
+
+<template>
+    <div class="flex flex-col">
+        <label class="inline-block mb-2 text-sm text-secondary leading-[14.22px] font-bold tracking-[1.13px]" for="input"> {{ label }}</label>
+
+        <input 
+            @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+            :type="inputType"
+            id="input"
+            :placeholder="placeholder"
+            required maxlength="500"
+            class="w-[315px] px-[15.5px] py-3 border-[1px] border-blue-100 rounded-md text-base 
+                text-input font-normal leading-[17.78px] placeholder:text-placeholder outline-blue-700"
+        />
+    </div>
+</template>
