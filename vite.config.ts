e47ce5074bd0e2550729aa255e852e30c2b0b40c
jamshid <jamshidtulaganov3101@gmail.com>
2023-09-25 15:37:18 +0500

    feat: vue typescript checker plugin installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      vueTsc: true
+    })
+  ],
+
+//  server
+  server: {
+    host: true,
+    port: 3000
+  }
 })
